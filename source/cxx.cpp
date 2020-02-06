833cc91a1fe62c6e89240ab7317abbd0ee29d9f8
AECX <aecx@aecx.cc>
2020-02-06 15:34:47 +0100

    Fix namespace

--- source/cxx.cpp
+++ source/cxx.cpp
@@ -5,33 +5,33 @@
 
 void* operator new(size_t size)
 {
-    void* ArchiveHeapPtr = tp::m_Do_ext::archiveHeap;
-    void* NewPtr = tp::jkr_exp_heap::do_alloc_JKRExpHeap(ArchiveHeapPtr, size, 0x20);
+    void* ArchiveHeapPtr = libtp::tp::m_Do_ext::archiveHeap;
+    void* NewPtr = libtp::tp::jkr_exp_heap::do_alloc_JKRExpHeap(ArchiveHeapPtr, size, 0x20);
     return memset(NewPtr, 0, size);
 }
 void* operator new[](size_t size)
 {
-    void* ArchiveHeapPtr = tp::m_Do_ext::archiveHeap;
-    void* NewPtr = tp::jkr_exp_heap::do_alloc_JKRExpHeap(ArchiveHeapPtr, size, 0x20);
+    void* ArchiveHeapPtr = libtp::tp::m_Do_ext::archiveHeap;
+    void* NewPtr = libtp::tp::jkr_exp_heap::do_alloc_JKRExpHeap(ArchiveHeapPtr, size, 0x20);
     return memset(NewPtr, 0, size);
 }
 void operator delete(void* ptr)
 {
-    void* ArchiveHeapPtr = tp::m_Do_ext::archiveHeap;
-    return tp::jkr_exp_heap::do_free_JKRExpHeap(ArchiveHeapPtr, ptr);
+    void* ArchiveHeapPtr = libtp::tp::m_Do_ext::archiveHeap;
+    return libtp::tp::jkr_exp_heap::do_free_JKRExpHeap(ArchiveHeapPtr, ptr);
 }
 void operator delete[](void* ptr)
 {
-    void* ArchiveHeapPtr = tp::m_Do_ext::archiveHeap;
-    return tp::jkr_exp_heap::do_free_JKRExpHeap(ArchiveHeapPtr, ptr);
+    void* ArchiveHeapPtr = libtp::tp::m_Do_ext::archiveHeap;
+    return libtp::tp::jkr_exp_heap::do_free_JKRExpHeap(ArchiveHeapPtr, ptr);
 }
 void operator delete(void* ptr, std::size_t size)
 {
-    void* ArchiveHeapPtr = tp::m_Do_ext::archiveHeap;
-    return tp::jkr_exp_heap::do_free_JKRExpHeap(ArchiveHeapPtr, ptr);
+    void* ArchiveHeapPtr = libtp::tp::m_Do_ext::archiveHeap;
+    return libtp::tp::jkr_exp_heap::do_free_JKRExpHeap(ArchiveHeapPtr, ptr);
 }
 void operator delete[](void* ptr, std::size_t size)
 {
-    void* ArchiveHeapPtr = tp::m_Do_ext::archiveHeap;
-    return tp::jkr_exp_heap::do_free_JKRExpHeap(ArchiveHeapPtr, ptr);
+    void* ArchiveHeapPtr = libtp::tp::m_Do_ext::archiveHeap;
+    return libtp::tp::jkr_exp_heap::do_free_JKRExpHeap(ArchiveHeapPtr, ptr);
 }
