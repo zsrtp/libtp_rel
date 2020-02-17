3333369211e8fb0348838bc2343687c09a763325
AECX <aecx@aecx.cc>
2020-02-17 19:26:27 +0100

    indexOf implementation

--- include/tools.h
+++ include/tools.h
@@ -3,6 +3,8 @@
  *
  *	@author AECX
  *	@bug No known bugs.
+ *  @todo At some point we might want to seperate functions into different namespaces
+ * i.e. tools::array, tools::hashing, etc.; for now there aren't enough functions for this to make sense
  */
 
 #pragma once
@@ -11,5 +13,13 @@
 
 namespace libtp::tools
 {
-    s32 indexOf(void** arr, void* data, s32 chunkSize = sizeof(char));
-}
+    /**
+     *  @brief Returns the index of needle in haystack, -1 if error
+     *
+     *  @param haystack The array for which you need the index
+     *  @param needle The data to find in haystack
+     *  @param length Length (in bytes) of haystack
+     *  @param chunkSize Length (in bytes) of one individual element (haystack[0])
+     */
+    s32 indexOf(const void* haystack, void* needle, s32 length, s32 chunkSize = sizeof(char));
+}  // namespace libtp::tools
--- source/tools.cpp
+++ source/tools.cpp
@@ -1,18 +1,35 @@
 #include "tools.h"
 
+#include <cstdio>
 #include <cstring>
 
+#include "display/console.h"
+#include "memory.h"
 #include "types.h"
 
 namespace libtp::tools
 {
-    s32 indexOf(void** arr, void* data, s32 chunkSize)
+    s32 indexOf(const void* haystack, void* needle, s32 length, s32 chunkSize)
     {
-        s32 i = -1;
-        while (memcmp(arr[i], data, chunkSize))
+        // return, -1 = err
+        s32 r = -1;
+
+        // Helper variables
+        u32 baseAddress = reinterpret_cast<u32>(haystack);
+        s32 numElements = length / chunkSize;
+
+        sprintf(libtp::display::print(17, ""), "Size: %d", sizeof(haystack));
+
+        for (s32 n = 0; n < numElements; n++)
         {
-            i++;
+            // Compare n'th element of haystack with needle
+            if (!memcmp(reinterpret_cast<void*>(baseAddress + n * chunkSize), needle, chunkSize))
+            {
+                r = n;
+                break;
+            }
         }
-        return i;
+
+        return r;
     }
 }  // namespace libtp::tools
