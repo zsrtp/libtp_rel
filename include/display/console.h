ab3fd2f2f87a154c5c77c08a3977f8285e1589e9
AECX <aecx@aecx.cc>
2020-02-17 03:07:47 +0100

    Test console wrapper (should be improved at some point)

---
+++ include/display/console.h
@@ -0,0 +1,71 @@
+/**	@file console.h
+ *  @brief Wrapper functions for the
+ *
+ *  Provides definitions for using and interacting with the console class
+ *
+ *  @todo Create some sort of menuing system etc.
+ *	@author AECX
+ *	@bug No known bugs.
+ */
+
+#pragma once
+
+#include "../tp/JFWSystem.h"
+#include "../types.h"
+
+namespace libtp::display
+{
+    // Replace %s with title
+    const char* heading = "Twilight Princess - REL::%s";
+    const char* pre_description = "A mod by %s:";
+    const char* legalText = "Powered by libtp | (C) AECX, Zephiles";
+    const char* github = "Github: zsrtp";
+    const char* twitter = "Twitter: AECXTP, Zephiles";
+
+    /**
+     *  @brief Console wrapper class
+     *
+     *  @todo Add features (menu, watches, etc.)
+     */
+    class Console
+    {
+       public:
+        /**
+         *  @brief Wrapper class for accessing the JFW console
+         *
+         *  @param author Author name to display on legal stream
+         *  @param title The name of your mod
+         *  @param description Small description text about your mod
+         *  @param version Version string (i.e. v3.1)
+         */
+        Console(const char* author, const char* title, const char* description, const char* version);
+
+       private:
+        const char* _author;       // Author name to display on legal screen
+        const char* _title;        // Title of this mod
+        const char* _description;  // Short description
+        const char* _version;      // Version string to display (i.e. v3.1)
+    };
+    /**
+     *  @brief Sets the state of the console
+     *
+     *  @param state If true: Enabled otherwise disabled
+     *  @param lines Number of lines to enable, 0 to enable the console but hide the lines (max. 25; check: tp/JFWSystem.h)
+     */
+    void setConsole(bool state, u8 lines);
+
+    /**
+     *  @brief Sets the console background colour + Alpha
+     */
+    void setConsoleColor(u8 red, u8 green, u8 blue, u8 alpha);
+
+    /**
+     *  @brief Copies text to a console line
+     *
+     *  @param line The targeted line
+     *  @param text The text to be printed
+     *
+     *  @return The pointer to the console line
+     */
+    char* print(u8 line, const char* text);
+}  // namespace libtp::display
---
+++ source/display/console.cpp
@@ -0,0 +1,55 @@
+#include "display/console.h"
+
+#include <cstdio>
+#include <cstring>
+
+#include "tp/JFWSystem.h"
+#include "types.h"
+
+namespace libtp::display
+{
+    Console::Console(const char* author, const char* title, const char* description, const char* version)
+        : _author(author), _title(title), _description(description), _version(version)
+    {
+        // Display some information about this mod, the project and everything
+
+        tp::jfw_system::SystemConsole* console = tp::jfw_system::systemConsole;
+        std::size_t max = sizeof(tp::jfw_system::ConsoleLine::line);
+
+        setConsole(true, 25);
+
+        snprintf(console->consoleLine[0].line, max, heading, _title);
+        snprintf(console->consoleLine[1].line, max, pre_description, _author);
+        strncpy(console->consoleLine[3].line, _description, max);
+        strncpy(console->consoleLine[10].line, _version, max);
+        strncpy(console->consoleLine[21].line, legalText, max);
+        strncpy(console->consoleLine[23].line, github, max);
+        strncpy(console->consoleLine[24].line, twitter, max);
+
+        return;
+    }
+
+    void setConsole(bool state, u8 lines)
+    {
+        // Load the console as a local pointer to avoid loading it each loop
+        tp::jfw_system::SystemConsole* console = tp::jfw_system::systemConsole;
+
+        console->consoleEnabled = state;
+
+        for (u8 line = 0; lines < lines; line++)
+        {
+            console->consoleLine[line].showLine = state;
+        }
+    }
+
+    void setConsoleColor(u8 red, u8 green, u8 blue, u8 alpha)
+    {
+        *tp::jfw_system::systemConsole->consoleColor = (u32)(red << 24 | green << 16 | blue << 8 | alpha);
+        return;
+    }
+
+    char* print(u8 line, const char* text)
+    {
+        return strncpy(tp::jfw_system::systemConsole->consoleLine[line].line, text, sizeof(tp::jfw_system::ConsoleLine::line));
+    }
+}  // namespace libtp::display
