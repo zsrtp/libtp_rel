4ec4afbfafea909cda075b3e4df85b6f6d2e839a
lunarsoap5 <40438826+lunarsoap5@users.noreply.github.com>
2021-01-12 18:59:41 -0600

    Add files via upload

--- include/tp/d_msg_object.h
+++ include/tp/d_msg_object.h
@@ -2,6 +2,7 @@
  *	@brief Contains some of the functions used in getting the values for a message
  *
  *	@author Lunar Soap
+ *  @author Zephiles
  *	@bug No known bugs.
  */
 #pragma once
@@ -9,6 +10,26 @@
 
 namespace libtp::tp::d_msg_object
 {
+    struct StringDataTableEntry
+    {
+        uint32_t offsetToString;
+        uint16_t stringId;
+        uint8_t unk[0xE];
+    } __attribute__((__packed__));
+
+    struct StringDataTable
+    {
+        uint8_t unk_0[0x20];
+        uint8_t unk_20[0x4];
+        uint32_t stringsStartOffset;
+        uint16_t totalEntries; // Should always be 5000
+        uint16_t entrySize; // Should always be 0x14
+        uint8_t unk_2c[0x4];
+        StringDataTableEntry entry[5000];
+    } __attribute__((__packed__));
+
+    static_assert(sizeof(StringDataTableEntry) == 0x14);
+
     extern "C"
     {
         /**
