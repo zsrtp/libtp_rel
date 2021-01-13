5b1e2c65c65d98ae2543217f226a82210e7b37c9
lunarsoap5 <40438826+lunarsoap5@users.noreply.github.com>
2021-01-12 20:19:48 -0600

    Added Zeph's structs to control.h, d_msg_object.h and d_meter2_info.h and removed unnecessary comment

---
+++ include/tp/control.h
@@ -0,0 +1,17 @@
+/**	@file control.h
+ *	@brief Contains functions for handling various aspects of the game
+ *
+ *  @author Zephiles
+ *	@author Lunar Soap
+ *	@bug No known bugs.
+ */
+#pragma once
+#include <cstdint>
+
+namespace libtp::tp::control
+{
+	extern "C"
+	{
+		bool render(void* TControl);
+	}
+} // namespace libtp::tp::control
--- include/tp/d_meter2_info.h
+++ include/tp/d_meter2_info.h
@@ -10,11 +10,21 @@
 
 namespace libtp::tp::d_meter2_info
 {
+    struct G_Meter2_Info
+    {
+        uint8_t unk_0[0x10];
+        tp::d_msg_object::StringDataTable* stringDataTable;
+        uint8_t unk_14[0xE4];
+    } __attribute__((__packed__));
+
+    static_assert(sizeof(G_Meter2_Info) == 0xF8);
+    
     extern "C"
     {
         /**
          *	@brief Gets a pointer to the transparancy of the Z button
          */
         extern void* wZButtonPtr;
+        extern G_Meter2_Info g_meter2_info;
     }
 }  // namespace libtp::tp::d_meter2_info
