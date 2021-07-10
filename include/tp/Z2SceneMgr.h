cd479e703c3c1b7374f5088d9e20300946631b91
jdflyer <jdflyer10@gmail.com>
2021-07-10 10:32:43 -0700

    clang format

--- include/tp/Z2SceneMgr.h
+++ include/tp/Z2SceneMgr.h
@@ -2,7 +2,7 @@
  * @file Z2SceneMgr.h
  * @author jdflyer
  * @brief Provides a funciton definition for the sceneChange function
- * 
+ *
  * @bug No known bugs
  */
 
@@ -11,22 +11,32 @@
 
 #include <cstdint>
 
-namespace libtp::tp::z2audiolib::z2scenemgr {
+namespace libtp::tp::z2audiolib::z2scenemgr
+{
     struct Z2SceneMgr;
 
     /**
      *@todo Move JAISoundID to its own file
      */
-    struct JAISoundID {
+    struct JAISoundID
+    {
         uint32_t id;
     };
 
-    extern "C" {
+    extern "C"
+    {
         /**
          * @brief sceneChange is called when the game changes the BGM playing
          */
-        void sceneChange(Z2SceneMgr* sceneMgr, JAISoundID id,uint8_t SeWave1,uint8_t SeWave2,uint8_t BgmWave1,uint8_t BgmWave2,uint8_t DemoWave,bool param_7);
+        void sceneChange( Z2SceneMgr* sceneMgr,
+                          JAISoundID id,
+                          uint8_t SeWave1,
+                          uint8_t SeWave2,
+                          uint8_t BgmWave1,
+                          uint8_t BgmWave2,
+                          uint8_t DemoWave,
+                          bool param_7 );
     };
-}   //namespace libtp::tp::z2audiolib::z2scenemgr
+}     // namespace libtp::tp::z2audiolib::z2scenemgr
 
 #endif
--- include/tp/Z2SeqMgr.h
+++ include/tp/Z2SeqMgr.h
@@ -2,7 +2,7 @@
  * @file Z2SeqMgr.h
  * @author jdflyer
  * @brief Provides the definition for the funciton that starts the enemy BGM
- * 
+ *
  * @bug No known bugs
  */
 #ifndef TP_Z2SEQMGR_H
@@ -10,12 +10,14 @@
 
 #include <cstdint>
 
-namespace libtp::tp::z2audiolib::z2seqmgr {
+namespace libtp::tp::z2audiolib::z2seqmgr
+{
     struct Z2SeqMgr;
 
-    extern "C" {
-        void startBattleBgm(Z2SeqMgr* seqMgr, bool param_1);
+    extern "C"
+    {
+        void startBattleBgm( Z2SeqMgr* seqMgr, bool param_1 );
     }
-}   //  namespace libtp::tp::z2audiolib::z2seqmgr
+}     //  namespace libtp::tp::z2audiolib::z2seqmgr
 
 #endif
