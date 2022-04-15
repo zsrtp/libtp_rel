0876e813272341b69d75287de81495ea4f238a97
lunarsoap5 <justindakotacarter@gmail.com>
2022-04-14 20:28:44 -0500

    Create d_a_player.h

---
+++ include/tp/d_a_player.h
@@ -0,0 +1,18 @@
+/**	@file d_a_player.h
+ *	@brief Contains functions and values that relate to the player and its interactions.
+ *
+ *	@author Lunar Soap
+ *	@bug No known bugs.
+ */
+#ifndef TP_D_A_PLAYER_H
+#define TP_D_A_PLAYER_H
+#include <cstdint>
+
+namespace libtp::tp::d_a_player
+{
+    extern "C"
+    {
+        void* m_midnaActor;
+    }
+}     // namespace libtp::tp::d_a_player
+#endif
