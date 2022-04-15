e26a305c2cb14dbac310e071353dfe8c2c3e1c9f
lunarsoap5 <justindakotacarter@gmail.com>
2022-04-14 20:47:15 -0500

    Create d_camera.h

---
+++ include/tp/d_camera.h
@@ -0,0 +1,20 @@
+/**	@file d_camera.h
+ *	@brief Contains functions and values that control how the camera behaves.
+ *
+ *	@author Lunar Soap
+ *	@bug No known bugs.
+ */
+#ifndef TP_D_CAMERA_H
+#define TP_D_CAMERA_H
+#include <cstdint>
+
+#include "tp/d_a_alink.h"
+
+namespace libtp::tp::d_camera
+{
+    extern "C"
+    {
+        bool checkRide( d_a_alink::daAlink );
+    }
+}     // namespace libtp::tp::d_camera
+#endif
