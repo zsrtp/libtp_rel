a9ed2699757d4e3fe5a4a465bc008da8f572aace
lunarsoap5 <justindakotacarter@gmail.com>
2022-04-17 00:39:06 -0500

    change to unsigned

--- include/tp/d_msg_flow.h
+++ include/tp/d_msg_flow.h
@@ -17,7 +17,7 @@ namespace libtp::tp::d_msg_flow
         bool query023( void* unk1, void* unk2, int32_t unk3 );
         bool query025( void* unk1, void* unk2, int32_t unk3 );
         bool query042( void* unk1, void* unk2, int32_t unk3 );
-        int event003( void* unk1, void* param_1, void* param_2 );
+        uint32_t event003( void* unk1, void* param_1, void* param_2 );
         void event035( void* unk1, void* param_1, void* param_2 );
         bool chkEvtBit( uint32_t flag );
         bool chkEvtBitF( uint32_t flag );
