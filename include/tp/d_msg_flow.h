3b6b9f68ca944c8b2f6360ec59c7efef168b12f2
lunarsoap5 <justindakotacarter@gmail.com>
2022-04-17 01:18:48 -0500

    Added event041 function definition.

--- include/tp/d_msg_flow.h
+++ include/tp/d_msg_flow.h
@@ -17,7 +17,12 @@ namespace libtp::tp::d_msg_flow
         bool query023( void* unk1, void* unk2, int32_t unk3 );
         bool query025( void* unk1, void* unk2, int32_t unk3 );
         bool query042( void* unk1, void* unk2, int32_t unk3 );
-        int event003( void* unk1, void* param_1, void* param_2 );
+        int event003( void* unk1,
+                      void* param_1,
+                      void* param_2 );     // Runs when you dontate to Charlo. Reduces the wallet by the donated amount.
+        int event041( void* unk1,
+                      void* param_1,
+                      void* param_2 );     // Runs when you donate to Charlo. Increases the amount donated.
         void event035( void* unk1, void* param_1, void* param_2 );
         bool chkEvtBit( uint32_t flag );
         bool chkEvtBitF( uint32_t flag );
