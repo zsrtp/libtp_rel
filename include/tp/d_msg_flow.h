f5b1fb1e0d49b97dffd87da6c73a3a71b6235f33
lunarsoap5 <justindakotacarter@gmail.com>
2022-04-17 01:41:07 -0500

    added query004 function definition.

--- include/tp/d_msg_flow.h
+++ include/tp/d_msg_flow.h
@@ -17,6 +17,7 @@ namespace libtp::tp::d_msg_flow
         bool query023( void* unk1, void* unk2, int32_t unk3 );
         bool query025( void* unk1, void* unk2, int32_t unk3 );
         bool query042( void* unk1, void* unk2, int32_t unk3 );
+        int query004( void* unk1, void* unk2, int32_t unk3 );
         int event003( void* unk1,
                       void* param_1,
                       void* param_2 );     // Runs when you dontate to Charlo. Reduces the wallet by the donated amount.
