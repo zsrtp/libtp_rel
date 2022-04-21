cd837e30050d22a73d1d6f234557cfa1b535a10a
lunarsoap5 <justindakotacarter@gmail.com>
2022-04-21 15:04:51 -0500

    added struct definitions

--- include/tp/d_resource.h
+++ include/tp/d_resource.h
@@ -11,9 +11,22 @@
 
 namespace libtp::tp::d_resource
 {
+    struct dRes_info_c
+    {
+        /* 0x00 */ char mArchiveName[11];
+        /* 0x0C */ uint16_t mCount;
+        /* 0x10 */ void* mDMCommand;     // mDoDvdThd_mountArchive_c*
+        /* 0x14 */ void* mArchive;       // JKRArchive*
+        /* 0x18 */ void* heap;           // JKRHeap*
+        /* 0x1C */ void* mDataHeap;      // JKRSolidHeap*
+        /* 0x20 */ void** mRes;
+    } __attribute__( ( __packed__ ) );
+
+    static_assert( sizeof( dRes_info_c ) == 0x24 );
+
     extern "C"
     {
-        void* getResInfo( const char* arcName, void* objectInfo, int size );
+        void* getResInfo( const char* arcName, dRes_info_c* objectInfo, int size );
     }
 }     // namespace libtp::tp::d_resource
 #endif
