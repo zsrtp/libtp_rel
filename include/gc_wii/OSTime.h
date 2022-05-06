00718258c62d48b84fefa90ace05614ac5511afc
lunarsoap5 <justindakotacarter@gmail.com>
2022-05-06 15:02:06 -0500

    added function definition for OSGetTick

---
+++ include/gc_wii/OSTime.h
@@ -0,0 +1,21 @@
+/**	@file OSTime.h
+ *	@brief Holds OSTimee definitions from dolphin OS
+ *
+ *	For additional information check the dolphin OS manual
+ *
+ *	@author Lunar Soap
+ *	@bug No known bugs.
+ */
+#ifndef GC_WII_OSTIME_H
+#define GC_WII_OSTIME_H
+
+#include <cstdint>
+
+namespace libtp::gc_wii::os_time
+{
+    extern "C"
+    {
+        extern uint32_t OSGetTick();
+    }
+}     // namespace libtp::gc_wii::os_time
+#endif
