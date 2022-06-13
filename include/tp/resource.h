a989612cb32ff2ae6bec09b511a266d083c26052
lunarsoap5 <justindakotacarter@gmail.com>
2022-06-12 22:33:57 -0500

    Changed kanji name and value for special character

--- include/tp/resource.h
+++ include/tp/resource.h
@@ -20,7 +20,7 @@
 #define MSG_OPTION_LIST( option ) "\x1A\x06\x00\x00\x09" option
 
 // Special Characters
-#define MSG_SP_CHAR_HAIKYO "\x94\x70\x1A\x05\x04\x00\x0A"
+#define MSG_SP_CHAR_KYO "\x1A\x05\x04\x00\x0A"
 
 // Message Speeds
 #define MSG_SPEED_FAST "\x01"
