8b08931e22e793f1da55489ac2dd81a32a5b431f
lunarsoap5 <40438826+lunarsoap5@users.noreply.github.com>
2023-01-08 19:52:44 -0600

    Create d_menu_fmap2D and added definition for checkPlayerWarpAccept

---
+++ include/tp/d_menu_fmap2D.h
@@ -0,0 +1,17 @@
+/**	@file d_menu_fmap2D.h
+ *	@brief Symbols of the d_menu_fmap2D field
+ *
+ *	@author Lunar Soap
+ *	@bug No known bugs.
+ */
+#ifndef TP_D_MENU_FMAP2D_H
+#define TP_D_MENU_FMAP2D_H
+
+namespace libtp::tp::d_menu_fmap2D
+{
+    extern "C"
+    {
+        bool checkPlayerWarpAccept( void* mapPtr );
+    }
+}     // namespace libtp::tp::d_menu_fmap2D
+#endif
