e7046c8e7da0e54b32bae676f7d24d051786e762
Zephiles <cae222@yahoo.com>
2023-02-19 19:01:34 -0600

    Fix return types for query functions

--- include/tp/d_msg_flow.h
+++ include/tp/d_msg_flow.h
@@ -13,10 +13,10 @@ namespace libtp::tp::d_msg_flow
 {
     extern "C"
     {
-        bool query022( void* unk1, void* unk2, int32_t unk3 );
+        int32_t query022( void* unk1, void* unk2, int32_t unk3 );
         int32_t query023( void* unk1, void* unk2, int32_t unk3 );
-        bool query025( void* unk1, void* unk2, int32_t unk3 );
-        bool query042( void* unk1, void* unk2, int32_t unk3 );
+        int32_t query025( void* unk1, void* unk2, int32_t unk3 );
+        int32_t query042( void* unk1, void* unk2, int32_t unk3 );
         int32_t query004( void* unk1, void* unk2, int32_t unk3 );
         int32_t query037( void* unk1, void* unk2, int32_t unk3 );
         int32_t query049( void* unk1, void* unk2, int32_t unk3 );
@@ -26,8 +26,8 @@ namespace libtp::tp::d_msg_flow
         int32_t event041( void* unk1,
                           void* param_1,
                           void* param_2 );     // Runs when you donate to Charlo. Increases the amount donated.
-        void event035( void* unk1, void* param_1, void* param_2 );
-        uint32_t event000( void* messageFlow, void* nodeEvent, void* actrPtr );
+        int32_t event035( void* unk1, void* param_1, void* param_2 );
+        int32_t event000( void* messageFlow, void* nodeEvent, void* actrPtr );
         int32_t event017( void* messageFlow, void* nodeEvent, void* actrPtr );
     }
 }     // namespace libtp::tp::d_msg_flow
