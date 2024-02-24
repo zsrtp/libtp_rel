4580be2ad0fbe0a6588231d3ee8d4e1903446a2a
Captain Kitty Cat <68467449+Captainkittyca2@users.noreply.github.com>
2024-02-24 11:29:51 +0300

    Update OSTime.h

--- include/gc_wii/OSTime.h
+++ include/gc_wii/OSTime.h
@@ -4,6 +4,7 @@
  *	For additional information check the dolphin OS manual
  *
  *	@author Lunar Soap
+ *  @author Captain Kitty Cat
  *	@bug No known bugs.
  */
 #ifndef GC_WII_OSTIME_H
@@ -13,9 +14,16 @@
 
 namespace libtp::gc_wii::os_time
 {
+    struct GCTime
+    {
+        uint32_t unk1;
+    } __attribute__((__packed__));
+
     extern "C"
     {
+        extern GCTime __OSBusClock;
         uint32_t OSGetTick();
+        int64_t OSGetTime();
     }
 } // namespace libtp::gc_wii::os_time
 #endif
