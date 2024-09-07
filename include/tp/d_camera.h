18e326c554bf0c35f1305e5048f4f7b99e17c601
Luzagar <luzagar.abonner@gmail.com>
2024-09-07 11:36:51 +0200

    change all int to int32_t

--- include/tp/d_camera.h
+++ include/tp/d_camera.h
@@ -72,19 +72,19 @@ namespace libtp::tp::d_camera
                     /* 0x10 */ cXyz field_0x10;
                     /* 0x1C */ void* field_0x1c;
                     /* 0x1E */ void* field_0x1e;
-                    /* 0x20 */ int field_0x20;
+                    /* 0x20 */ int32_t field_0x20;
                     /* 0x24 */ void* field_0x24;
                     /* 0x2C */ void* field_0x2c;
-                    /* 0x34 */ int field_0x34;
+                    /* 0x34 */ int32_t field_0x34;
                     /* 0x38 */ float field_0x38;
-                    /* 0x3C */ int field_0x3c;
-                    /* 0x40 */ int field_0x40;
-                    /* 0x44 */ int field_0x44;
+                    /* 0x3C */ int32_t field_0x3c;
+                    /* 0x40 */ int32_t field_0x40;
+                    /* 0x44 */ int32_t field_0x44;
                 } /* 0x0C0 */ field_0xc0;
                 class
                 {
                    public:
-                    /* 0x0 */ int field_0x0;
+                    /* 0x0 */ int32_t field_0x0;
                     /* 0x4 */ float field_0x4;
                     /* 0x8 */ cXyz field_0x8;
                 } /* 0x108 */ field_0x108;
@@ -93,8 +93,8 @@ namespace libtp::tp::d_camera
             struct Shake
             {
                 /* 0x00 */ uint8_t field_0x0[12];
-                /* 0x0C */ int field_0xc;
-                /* 0x10 */ int field_0x10;
+                /* 0x0C */ int32_t field_0xc;
+                /* 0x10 */ int32_t field_0x10;
                 /* 0x14 */ uint8_t field_0x14[4];
                 /* 0x18 */ cXyz field_0x18;
                 /* 0x24 */ cXyz field_0x24;
@@ -104,12 +104,12 @@ namespace libtp::tp::d_camera
             }; // Size: 0x44
 
             /* 0x000 */ void** field_0x0;
-            /* 0x004 */ int mRoomNo;
+            /* 0x004 */ int32_t mRoomNo;
             /* 0x008 */ uint8_t field_0x8[0x20 - 0x8];
             /* 0x020 */ uint8_t field_0x20;
             /* 0x021 */ uint8_t field_0x21;
             /* 0x022 */ uint8_t field_0x22[2];
-            /* 0x024 */ int field_0x24;
+            /* 0x024 */ int32_t field_0x24;
             /* 0x028 */ void* field_0x28;
             /* 0x030 */ cXyz mCenter;
             /* 0x03C */ cXyz mEye;
@@ -131,14 +131,14 @@ namespace libtp::tp::d_camera
             /* 0x08C */ void* field_0x8c;
             /* 0x090 */ cXyz field_0x90;
             /* 0x09C */ cXyz field_0x9c;
-            /* 0x0A8 */ int field_0xa8;
+            /* 0x0A8 */ int32_t field_0xa8;
             /* 0x0AC */ uint32_t field_0xac;
             /* 0x0B0 */ dCamInfo_c field_0xb0;
             /* 0x0D0 */ dCamInfo_c field_0xd0[2];
             class
             {
                public:
-                /* 0x00 */ int field_0x0;
+                /* 0x00 */ int32_t field_0x0;
                 /* 0x04 */ uint16_t field_0x4;
                 /* 0x08 */ dCamInfo_c field_0x8;
                 /* 0x28 */ cXyz field_0x28;
@@ -159,30 +159,30 @@ namespace libtp::tp::d_camera
                 /* 0x4 */ void* field_0x4;
             }
             /* 0x158 */ field_0x158;
-            /* 0x160 */ int field_0x160;
+            /* 0x160 */ int32_t field_0x160;
             /* 0x164 */ uint8_t field_0x164[0x170 - 0x164];
-            /* 0x170 */ int field_0x170;
+            /* 0x170 */ int32_t field_0x170;
             /* 0x174 */ uint32_t mCurCamTypeTimer;
             /* 0x178 */ uint32_t mCameraID;
             /* 0x17C */ uint32_t mPadID;
             /* 0x180 */ libtp::tp::f_op_actor::fopAc_ac_c* mpPlayerActor;
             /* 0x184 */ libtp::tp::f_op_actor::fopAc_ac_c* mpLockonTarget;
             /* 0x188 */ uint8_t field_0x188[0x190 - 0x188];
-            /* 0x190 */ int field_0x190;
+            /* 0x190 */ int32_t field_0x190;
             /* 0x194 */ uint8_t field_0x194;
             /* 0x198 */ uint32_t field_0x198;
             /* 0x19C */ uint32_t field_0x19c;
-            /* 0x1A0 */ int mCurMode;
-            /* 0x1A4 */ int mNextMode;
-            /* 0x1A8 */ int field_0x1a8;
-            /* 0x1AC */ int field_0x1ac;
+            /* 0x1A0 */ int32_t mCurMode;
+            /* 0x1A4 */ int32_t mNextMode;
+            /* 0x1A8 */ int32_t field_0x1a8;
+            /* 0x1AC */ int32_t field_0x1ac;
             /* 0x1B0 */ void* field_0x1b0;
             /* 0x1B4 */ float field_0x1b4;
             /* 0x1B8 */ uint8_t field_0x1b8[4];
             /* 0x1BC */ dCamera_pad_info mPadInfo;
-            /* 0x1F4 */ int field_0x1f4;
-            /* 0x1F8 */ int field_0x1f8;
-            /* 0x1FC */ int field_0x1fc;
+            /* 0x1F4 */ int32_t field_0x1f4;
+            /* 0x1F8 */ int32_t field_0x1f8;
+            /* 0x1FC */ int32_t field_0x1fc;
             /* 0x200 */ float mTriggerLeftLast;
             /* 0x204 */ float mTriggerLeftDelta;
             /* 0x208 */ uint8_t field_0x208;
@@ -212,12 +212,12 @@ namespace libtp::tp::d_camera
             /* 0x298 */ void* mFakeAngleSys;
             /* 0x2A0 */ void* mMonitor;
             /* 0x2C0 */ float field_0x2c0;
-            /* 0x2C4 */ int field_0x2c4;
+            /* 0x2C4 */ int32_t field_0x2c4;
             /* 0x2C8 */ float field_0x2c8;
             /* 0x2CC */ BG mBG;
             /* 0x3E8 */ uint8_t field_0x3e8;
-            /* 0x3EC */ int field_0x3ec;
-            /* 0x3F0 */ int field_0x3f0;
+            /* 0x3EC */ int32_t field_0x3ec;
+            /* 0x3F0 */ int32_t field_0x3f0;
             /* 0x3F4 */ uint8_t field_0x3f4[0x4E8 - 0x3F4];
             /* 0x4E8 */ void* mEventData;
             /* 0x60C */ uint32_t mEventFlags;
@@ -233,31 +233,31 @@ namespace libtp::tp::d_camera
             }
             /* 0x614 */ field_0x614[4];
             /* 0x664 */ uint8_t field_0x664[4];
-            /* 0x668 */ int field_0x668;
-            /* 0x66C */ int field_0x66c;
-            /* 0x670 */ int field_0x670;
-            /* 0x674 */ int field_0x674;
+            /* 0x668 */ int32_t field_0x668;
+            /* 0x66C */ int32_t field_0x66c;
+            /* 0x670 */ int32_t field_0x670;
+            /* 0x674 */ int32_t field_0x674;
             /* 0x678 */ uint8_t field_0x678;
-            /* 0x67C */ int mCamStyle;
-            /* 0x680 */ int field_0x680;
+            /* 0x67C */ int32_t mCamStyle;
+            /* 0x680 */ int32_t field_0x680;
             /* 0x684 */ void** mCamTypeData;
-            /* 0x688 */ int mCamTypeNum;
-            /* 0x68C */ int mCurType;
-            /* 0x690 */ int mNextType;
-            /* 0x694 */ int mMapToolType;
-            /* 0x698 */ int field_0x698;
-            /* 0x69C */ int field_0x69c;
+            /* 0x688 */ int32_t mCamTypeNum;
+            /* 0x68C */ int32_t mCurType;
+            /* 0x690 */ int32_t mNextType;
+            /* 0x694 */ int32_t mMapToolType;
+            /* 0x698 */ int32_t field_0x698;
+            /* 0x69C */ int32_t field_0x69c;
             /* 0x6A0 */ uint8_t field_0x6a0[0x6b8 - 0x6a0];
             /* 0x6B8 */ Shake mShake;
-            /* 0x6FC */ int field_0x6fc;
+            /* 0x6FC */ int32_t field_0x6fc;
             class
             {
                public:
                 /* 0x00 */ uint8_t field_0x0[4];
-                /* 0x04 */ int field_0x4;
+                /* 0x04 */ int32_t field_0x4;
                 /* 0x08 */ csXyz field_0x8;
                 /* 0x10 */ libtp::tp::f_op_actor::fopAc_ac_c* field_0x10;
-                /* 0x14 */ int field_0x14;
+                /* 0x14 */ int32_t field_0x14;
                 /* 0x18 */ cXyz mBlurePosition;
                 /* 0x24 */ cXyz mBlureScale;
             }
@@ -292,19 +292,19 @@ namespace libtp::tp::d_camera
             /* 0x914 */ uint8_t field_0x914[0x91c - 0x914];
             /* 0x91C */ float field_0x91c;
             /* 0x920 */ float mTrimHeight;
-            /* 0x924 */ int mTrimSize;
-            /* 0x928 */ int mTrimTypeForce;
+            /* 0x924 */ int32_t mTrimSize;
+            /* 0x928 */ int32_t mTrimTypeForce;
             /* 0x92C */ uint8_t field_0x92c[0x934 - 0x92c];
             /* 0x934 */ float field_0x934;
             /* 0x938 */ uint8_t field_0x938[0x93C - 0x938];
-            /* 0x93C */ int field_0x93c;
-            /* 0x940 */ int field_0x940;
+            /* 0x93C */ int32_t field_0x93c;
+            /* 0x940 */ int32_t field_0x940;
             /* 0x944 */ uint8_t field_0x944;
-            /* 0x948 */ int field_0x948;
+            /* 0x948 */ int32_t field_0x948;
             /* 0x94C */ void* field_0x94c;
-            /* 0x950 */ int field_0x950;
+            /* 0x950 */ int32_t field_0x950;
             /* 0x954 */ uint8_t field_0x954[0x958 - 0x954];
-            /* 0x958 */ int field_0x958;
+            /* 0x958 */ int32_t field_0x958;
             /* 0x95C */ cXyz field_0x95c;
             /* 0x968 */ uint8_t field_0x968[0x970 - 0x968];
             /* 0x970 */ void* mCamSetup;
