c35f2aef471e574a2f076d439d4f5abaf7885a15
lunarsoap5 <justindakotacarter@gmail.com>
2024-09-13 23:46:36 -0500

    Add function definitions for decideDoStatus as well as some item functions

---
+++ include/tp/d_a_obj_item.h
@@ -0,0 +1,45 @@
+/**	@file d_a_obj_item.h
+ *	@brief Holds structs and functions that deal with item actors.
+ *
+ *	@author Lunar Soap
+ *	@bug No known bugs.
+ */
+#ifndef TP_D_A_OBJ_ITEM_H
+#define TP_D_A_OBJ_ITEM_H
+
+#include <cstdint>
+
+namespace libtp::tp::d_a_obj_item
+{
+    extern "C"
+    {
+        /**
+         *  @brief Calls the appropriate functions to give the player an item based on the actor.
+         *
+         *  @param item A pointer to the Item actor being referenced.
+         */
+        void itemGet(void* item);
+
+        /**
+         *  @brief Initializes the events necessary for collecting an item based on the actor.
+         *
+         *  @param item A pointer to the Item actor being referenced.
+         */
+        void itemGetNextExecute(void* item);
+
+        /**
+         *  @brief Runs code that determines how an item acts while stationary.
+         *
+         *  @param item A pointer to the Item actor being referenced.
+         */
+        void mode_wait(void* item);
+
+        /**
+         *  @brief Initializes values in the item system and the specified actor.
+         *
+         *  @param item A pointer to the Item actor being referenced.
+         */
+        void CreateInit(void* item);
+    }
+} // namespace libtp::tp::d_a_obj_item
+#endif
