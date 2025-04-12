80eaca311aab5f04ed58601abcd47a70e96e570d
lunarsoap5 <justindakotacarter@gmail.com>
2025-04-12 17:40:28 -0500

    Add d_s_play file def

---
+++ include/tp/d_s_play.h
@@ -0,0 +1,20 @@
+/**	@file d_s_play.h
+ *	@brief Contains scene function definitions
+ *
+ *	@author Lunar Soap
+ *	@bug No known bugs.
+ */
+#ifndef TP_D_S_PLAY_H
+#define TP_D_S_PLAY_H
+
+#include <cstdint>
+
+namespace libtp::tp::d_s_play
+{
+
+    extern "C"
+    {
+        int32_t dScnPlay_phase_1();
+    }
+} // namespace libtp::tp::d_s_play
+#endif
