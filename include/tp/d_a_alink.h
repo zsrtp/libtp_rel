47c84fdcb67ab75d777c16fe385c6a9af03be9bd
lunarsoap5 <justindakotacarter@gmail.com>
2026-05-28 13:42:14 -0500

    Add definition for checkKandelaarSwing

--- include/tp/d_a_alink.h
+++ include/tp/d_a_alink.h
@@ -1150,6 +1150,13 @@ namespace libtp::tp::d_a_alink
         void setLightningSwordEffect(daAlink* player);
 
         /**
+         *	@brief Checks if the player is swinging the lantern
+         *
+         *  @return 1 if condition meets value 0 if not
+         */
+        uint32_t checkKandelaarSwing(daAlink* player, int32_t value);
+
+        /**
          *	@brief Determines whether Link is in heavy state (Iron boots, Ball and
          *Chain, ...)
          *
